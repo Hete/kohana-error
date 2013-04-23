12f07b656be7a5a24a55c0886136c3e603693f1b
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2013-04-23 19:53:08 -0400

    Fix for the route.

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 defined('SYSPATH') or die('No direct access');
 
 Route::set('error', 'error/<action>(/<description>)', array(
-    'action' => '[0-9]++', 'message' => '.+'
+    'action' => '[0-9]++', 'description' => '.+'
 ))->defaults(array(
     'controller' => 'error'
 ));
